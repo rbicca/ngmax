baa37207fdc0b03050d97a28f78d495c4484bf6b
Ronaldo Bicca <kuka@MacBook-de-Ronaldo.local>
2018-12-16 20:55:13 -0200

    Lesson 41 done

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,9 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
 
   constructor() { }
 
@@ -19,7 +22,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
 }
